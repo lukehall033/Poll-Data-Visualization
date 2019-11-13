764979ee2d124467ec5b4c43145885540c41b26c
lukehall033 <51924957+lukehall033@users.noreply.github.com>
2019-11-13 14:35:09 -0500

    Adding the song class

---
+++ Project 5/src/MuisicPollSorter/Song.java
@@ -0,0 +1,69 @@
+package MuisicPollSorter;
+
+public class Song {
+
+    private String name;
+    private String artist;
+    private int year;
+    private String genre;
+    
+    public Song(String songName, String songArtist, int songYear, String songGenre)
+    {
+        name = songName;
+        artist = songArtist;
+        year = songYear;
+        genre = songGenre;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }
+    
+    public String getArtist()
+    {
+        return this.artist;
+    }
+    
+    public int getYear()
+    {
+        return this.year;
+    }
+    
+    public String getGenre()
+    {
+        return this.genre;
+    }
+    
+    public String toString()
+    {
+        return (getName() + " by: " + 
+            getArtist() + ", released: " + getYear() + 
+            " in genre " +  getGenre());
+    }
+    
+    public boolean equals(Object obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+        if (obj == this)
+        {
+            return true;
+        }
+        if (this.getClass() == obj.getClass())
+        {
+            Song song = (Song)obj;
+            return (this.getName().equals(song.getName()) 
+                && this.getArtist().equals(song.getArtist()) && 
+                this.getYear() == song.getYear() && 
+                this.getGenre().equals(song.getGenre()));
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+}
