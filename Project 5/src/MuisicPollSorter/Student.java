7ccb5aa10440e403d0a6e23f318eccbdc2f35512
awoods99 <awoods99@users.noreply.github.com>
2019-11-14 17:57:44 -0500

    this commit is like one comment and formatting

--- Project 5/src/MuisicPollSorter/Student.java
+++ Project 5/src/MuisicPollSorter/Student.java
@@ -1,4 +1,5 @@
 package MuisicPollSorter;
+
 import java.util.ArrayList;
 
 public class Student {
@@ -9,9 +10,15 @@ public class Student {
     private String region;
     private String hobby;
     private ArrayList<String> responses;
-    
-    public Student(int studentID, String submitDate, String studentMajor, String studentRegion, String studentHobby, ArrayList<String> studentResponses)
-    {
+
+
+    public Student(
+        int studentID,
+        String submitDate,
+        String studentMajor,
+        String studentRegion,
+        String studentHobby,
+        ArrayList<String> studentResponses) {
         id = studentID;
         date = submitDate;
         major = studentMajor;
@@ -19,66 +26,61 @@ public class Student {
         hobby = studentHobby;
         responses = studentResponses;
     }
-    
-    public int getID()
-    {
+
+
+    public int getID() {
         return this.id;
     }
-    
-    public String getDate()
-    {
+
+
+    public String getDate() {
         return this.date;
     }
-    
-    public String getMajor()
-    {
+
+
+    public String getMajor() {
         return this.major;
     }
-    
-    public String getRegion()
-    {
+
+
+    public String getRegion() {
         return this.region;
     }
-    
-    public String getHobby()
-    {
+
+
+    public String getHobby() {
         return this.hobby;
     }
-    
-    public ArrayList<String> getResponses()
-    {
+
+
+    public ArrayList<String> getResponses() {
         return this.responses;
     }
-    
-    public String toString()
-    {
-        return ("Student " + getID() + " submitted on: " + 
-            getDate() + " and is a " + getMajor() + " major from " 
-            + getRegion() + " region and whos hobby is " + getHobby());
+
+
+    public String toString() {
+        return ("Student " + getID() + " submitted on: " + getDate()
+            + " and is a " + getMajor() + " major from " + getRegion()
+            + " region and whos hobby is " + getHobby());
     }
-    
-    public boolean equals(Object obj)
-    {
-        if (obj == null)
-        {
+
+
+    public boolean equals(Object obj) {
+        if (obj == null) {
             return false;
         }
-        if (obj == this)
-        {
+        if (obj == this) {
             return true;
         }
-        if (this.getClass() == obj.getClass())
-        {
+        if (this.getClass() == obj.getClass()) {
             Student student = (Student)obj;
-            return (this.getID() == student.getID() 
-                && this.getDate().equals(student.getDate()) && 
-                this.getMajor().equals(student.getMajor()) && 
-                this.getRegion().equals(student.getRegion()) && 
-                this.getHobby().equals(student.getHobby()) && 
-                this.getResponses().equals(student.getResponses()));
+            return (this.getID() == student.getID() && this.getDate().equals(
+                student.getDate()) && this.getMajor().equals(student.getMajor())
+                && this.getRegion().equals(student.getRegion()) && this
+                    .getHobby().equals(student.getHobby()) && this
+                        .getResponses().equals(student.getResponses()));
         }
-        else
-        {
+        else {
             return false;
         }
     }
