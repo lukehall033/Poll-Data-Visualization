7b34317fb47472d74aad3de5227521ca2291e117
lukehall033 <51924957+lukehall033@users.noreply.github.com>
2019-11-16 17:30:31 -0500

    adding the musicwindow for GUI submission for monday

---
+++ Project 5/src/MuisicPollSorter/MusicWindow.java
@@ -0,0 +1,21 @@
+package MuisicPollSorter;
+import java.awt.Color;
+import java.util.ArrayList;
+import CS2114.Button;
+import CS2114.Window;
+import CS2114.CircleShape;
+import CS2114.Shape;
+import CS2114.TextShape;
+import CS2114.WindowSide;
+
+public class MusicWindow {
+
+    private Window window;
+    private Button previous;
+    private Button next;
+    
+    public MusicWindow()
+    {
+        
+    }
+}
