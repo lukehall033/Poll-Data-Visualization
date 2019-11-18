3499e114fa4af965c34dcbb5a225afb9d0b944c5
Samk188 <57775830+Samk188@users.noreply.github.com>
2019-11-18 18:45:46 -0500

    added enum

---
+++ Project 5/src/prj5/Position.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package prj5;
+
+/**
+ * @author samkahn1
+ *
+ */
+public enum Position {
+    TOPLEFT,
+    TOPMIDDLE,
+    TOPRIGHT,
+    MIDDLELEFT,
+    MIDDLEMIDDLE,
+    MIDDLERIGHT,
+    BOTTOMLEFT,
+    BOTTOMMIDDLE,
+    BOTTOMRIGHT
+
+}
