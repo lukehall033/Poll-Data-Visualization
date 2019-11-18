395c0e97674de8b0e50d9f667ef0bc0d06ec2481
lukehall033 <51924957+lukehall033@users.noreply.github.com>
2019-11-17 19:30:26 -0500

    almost done with functionality for first test

--- Project 5/src/MuisicPollSorter/MusicCalculator.java
+++ Project 5/src/MuisicPollSorter/MusicCalculator.java
@@ -29,14 +29,41 @@ public class MusicCalculator {
         Arrays.sort(sortedSongList);
         for (int i = 0; i < sortedSongList.length; i++)
         {
+            int heardReading = 0;
+            int heardArt = 0;
+            int heardSports = 0;
+            int heardMusic = 0;
+            int likedReading = 0;
+            int likedArt = 0;
+            int likedSports = 0;
+            int likedMusic = 0;
+            for (int j = 0; j < studentList.getSize(); j++)
+            {
+                if (studentList.get(j + 1).getHobby().equals("reading"))
+                {
+                    //not done yet, need to tally everything up, but its almost done
+                }
+                else if (studentList.get(j + 1).getHobby().equals("art"))
+                {
+                    //not done yet
+                }
+                else if (studentList.get(j + 1).getHobby().equals("sports"))
+                {
+                    //not done yet
+                }
+                else
+                {
+                    //not done yet
+                }
+            }
             System.out.println("Song Title: " + sortedSongList[i].getName());
             System.out.println("Song Artist: " + sortedSongList[i].getArtist());
             System.out.println("Song Genre: " + sortedSongList[i].getGenre());
             System.out.println("Song Year: " + sortedSongList[i].getYear());
             System.out.println("Heard");
-            System.out.println("reading:0 art:0 sports:0 music:0");
+            System.out.println("reading:" + heardReading + " art:" + heardArt + " sports:" + heardSports + " music:" + heardMusic);
             System.out.println("Likes");
-            System.out.println("reading:0 art:0 sports:0 music:0");
+            System.out.println("reading:" + likedReading + " art:" + likedArt + " sports:" + likedSports + " music:" + likedMusic);
             System.out.println();
         }
     }
