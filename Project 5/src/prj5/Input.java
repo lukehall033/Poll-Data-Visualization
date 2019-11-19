9c9bdcc88b7baf80dd52a234fc83f66bae24c7eb
lukehall033 <51924957+lukehall033@users.noreply.github.com>
2019-11-18 19:34:17 -0500

    all tests work

--- Project 5/src/prj5/Input.java
+++ Project 5/src/prj5/Input.java
@@ -7,7 +7,7 @@ public class Input {
         //@SuppressWarnings("unused")
         //MusicReader reader = new MusicReader("SongList2019F.csv", "MusicSurveyData2019F.csv");    
         MusicReader reader = new MusicReader(args[1], args[0]);
-        //MusicReader reader = new MusicReader("SongList2018LessSongs.csv", "MusicSurveyData2018HolesLess.csv");
+        //MusicReader reader = new MusicReader("SongList2018HalfSongs.csv", "MusicSurveyData2018HolesHalf.csv");
     }
 
 }
